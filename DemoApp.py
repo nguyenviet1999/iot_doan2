d6cfe26ff6c880bd5ca5b3035a4b18a96ec1892a
nguyenviet1999 <59770345+nguyenviet1999@users.noreply.github.com>
2020-12-25 16:57:28 +0700

    Add files via upload

--- DemoApp.py
+++ DemoApp.py
@@ -1,7 +1,10 @@
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import Screen,ScreenManager
-from kivy.lang import Builder
-#Builder String
+from kivy.lang import Builder 
+from kivy.config import Config
+from kivy.core.window import Window
+Window.size = (312, 534)
+#Builder String 
 helper = '''
 ScreenManager:
     Hello:
@@ -23,8 +26,55 @@ ScreenManager:
                             text: 'Living Room'
                             icon: 'folder-home'
                             Image:
-                                pos_hint: {'center_x': .5, 'center_y': .5}
+                                size_hint: 1,1
+                                pos_hint: {'center_x': .5, 'center_y': .8}
                                 source: 'living.jpg'
+                            MDCard:
+                                orientation: 'vertical'
+                                pos_hint: {'center_x': .3, 'center_y': .3}
+                                size_hint: .25,.25
+                                pos: 70,100
+                                elevation: 10
+                                MDLabel:
+                                    text: "Temperature"
+                                    halign: 'center'
+                                    pos_hint: {'center_x': .5, 'center_y':.8}
+                                    theme_text_color: 'Custom'
+                                    text_color : 46/255, 123/255, 50/255,1
+                                    bold: True
+                                    font_size : '13'
+                           
+                                MDLabel:
+                                    text: "30"
+                                    bold: True
+                                    halign: 'center'
+                                    pos_hint: {'center_x': .5, 'center_y':.5}
+                                    font_size : '25'
+                                    theme_text_color: 'Custom'
+                                    text_color: 51/255, 51/255, 51/255,1
+                            MDCard:
+                                orientation: 'vertical'
+                                pos_hint: {'center_x': .7, 'center_y': .3}
+                                size_hint: .25,.25
+                                pos: 230,100
+                                elevation: 10
+                                MDLabel:
+                                    text: "Humidity"
+                                    halign: 'center'
+                                    pos_hint: {'center_x': .5, 'center_y':.8}
+                                    theme_text_color: 'Custom'
+                                    text_color : 46/255, 123/255, 50/255,1
+                                    bold: True
+                                    font_size : '13'
+                           
+                                MDLabel:
+                                    text: "85"
+                                    bold: True
+                                    halign: 'center'
+                                    pos_hint: {'center_x': .5, 'center_y':.5}
+                                    font_size : '25'
+                                    theme_text_color: 'Custom'
+                                    text_color: 51/255, 51/255, 51/255,1
                         MDBottomNavigationItem:
                             name: 'screen 2'
                             text: 'Bed Room'
@@ -41,6 +91,8 @@ ScreenManager:
                                 pos_hint: {'center_x': .5, 'center_y': .5}
                                 size: '150dp', '100dp'
                                 source: 'kitchen.jpg'
+                            
+           
         MDNavigationDrawer:
             id: nav_drawer
             BoxLayout:
@@ -51,41 +103,65 @@ ScreenManager:
                     source:'daidien.jpg'
                 MDLabel:
                     text: 'Nguyễn Văn Hoàng Việt'
-                    font_style: 'H5'
-                    halign: 'center'
+                    font_style: "Button"
                     size_hint_y: None
+                    halign: 'center'
+                    height: self.texture_size[1]
+                MDLabel:
+                    text: '106170276'
+                    halign: 'center'
+                    font_style: "Caption"
+                    size_hint_y: None
+                    height: self.texture_size[1]
                 ScrollView:
                     MDList:
                         OneLineIconListItem:
                             text: 'K157/28 Pham Nhu Xuong'
+                            font_style: "Caption"
                             IconLeftWidget:
                                 icon: 'home'
                         OneLineIconListItem:
                             text: 'Da Nang University'
+                            font_style: "Caption"
                             IconLeftWidget:
                                 icon: 'school'
                         OneLineIconListItem:
                             text:'0795565299'
+                            font_style: "Caption"
                             IconLeftWidget:
                                 icon: 'phone'
                         OneLineIconListItem:
+                            text:'Dark mode'
+                            font_style: "Caption"
+                            IconLeftWidget:
+                                icon: 'theme-light-dark'
                             MDSwitch:
                                 width: dp(40)
-                                pos_hint: {'center_x':.5,'center_y': .5}
+                                pos_hint: {'center_x':.7,'center_y': .5}
                                 on_active: app.check(*args)
 
 '''
 class Hello(Screen):
     pass
-
-
+class WindowManager(ScreenManager):
+    pass
+class MainWindow(Screen):
+    pass
 #sm = ScreenManager()
 
 #sm.add_widget(Hello(name = 'hello'))
 #sm.add_widget(Bye(name='bye'))
 
 class DemoApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    data = {
+        'lightbulb-on-outline':'',
+        'note': 'Remember to turn OFF',
+}
     def build(self):
+        self.theme_cls.primary_palette = 'Green'
+        self.theme_cls.primary_hue = '800'
  #       screen = Screen()
         screen = Builder.load_string(helper)
   #      screen.add_widget(help_str)
@@ -96,5 +172,7 @@ class DemoApp(MDApp):
             self.theme_cls.theme_style = 'Dark'
         else:
             self.theme_cls.theme_style = 'Light'
+    def callback(self, instance):
+        print(instance.icon)
 
 DemoApp().run()
